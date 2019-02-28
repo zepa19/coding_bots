27580de28aecb784779785b62b0fb0b0860b373a
DESKTOP-JH4EH5O\Damian <sushiken1@o2.pl>
2019-02-28 19:15:35 +0100

    + Reading input

--- coding_bots/Challenge.cs
+++ coding_bots/Challenge.cs
@@ -8,6 +8,34 @@ namespace coding_bots
 {
     public class Challenge
     {
+        List<Photo> photos;
+
+        class Photo
+        {
+            public static int count = 0;
+
+            public int ID;
+
+            public bool isHorizontal;
+
+            public List<string> Tags;
+
+            public Photo(string line)
+            {
+                ID = count;
+                count++;
+                var data = line.Split(' ');
+
+                isHorizontal = data[0] == "H";
+                int TagAmount = Int32.Parse(data[1]);
+                for(int i = 0; i < TagAmount; i++)
+                {
+                    Tags.Add(data[i + 2]);
+                }
+            }
+
+        }
+
         public bool PrepareData(List<string> lines)
         {
             if(lines == null)
@@ -15,7 +43,10 @@ namespace coding_bots
                 return false;
             }
 
-            // parsing input data
+            for (int i = 1; i < lines.Count(); i++)
+            {
+                photos.Add(new Photo(lines[i]));
+            }
 
             return true;
         }
